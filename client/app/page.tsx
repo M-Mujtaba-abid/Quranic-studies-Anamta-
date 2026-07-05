b674649b5a66e2fee47f13dca077de3576b0b5b7
MNoman431 <nomanrehann112233@gmail.com>
2026-07-05 18:54:09 +0500

    remove faq componenet

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
       <Teachers />
       <Testimonials />
       {/* <PricingPreview /> */}
-      <FAQ />
+      {/* <FAQ /> */}
       <CTABanner />
       <Footer />
     </main>
