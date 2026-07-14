41150644908d225c701fb7323112e19bfee1b196
Mujtaba Abid <mujtabaabid06@gmail.com>
2026-07-14 15:55:34 +0500

    update cors :1

--- server/src/main.ts
+++ server/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://localhost:3001',
       'https://quranic-studies-anamta-1eaq.vercel.app',
-      'https://quranic-studies-anamta.vercel.app'
+      'https://quranic-studies-anamta.vercel.app',
       'https://anamata.live',
       'https://www.anamata.live',
     ],
